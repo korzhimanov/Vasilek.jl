0de536d8dc791b8c1017d8572b29f0d1ceef378f
Artem Korzhimanov <korzhimanov.artem@gmail.com>
2020-04-11 20:01:48 +0300

    Rewrite Poisson.Fourier1D.solve_poisson!() to calculate frequencies by itself. Add test with sine function

--- src/MaxwellSolver/PoissonFourier1D.jl
+++ src/MaxwellSolver/PoissonFourier1D.jl
@@ -3,8 +3,10 @@ export solve_poisson!
 
 using FFTW
 
-function solve_poisson!(e, ω, ρ, Δx)
+function solve_poisson!(e, ρ, Δx)
     F = FFTW.rfft(ρ)
+    ω = collect(LinRange(0.0, π/Δx, length(F)))
+    ω[1] = ω[2]
     φ = FFTW.irfft(F./(-ω.^2), length(ρ))
     e[:] = vcat(0.5*[φ[2]-φ[end]], 0.5*(φ[3:end] - φ[1:end-2]), 0.5*[φ[1]-φ[end-1]])./Δx
 end
--- test/MaxwellSolver/test_poisson_fourier_1d.jl
+++ test/MaxwellSolver/test_poisson_fourier_1d.jl
@@ -1,5 +1,11 @@
-# include(joinpath("..","..","srс","MaxwellSolver","PoissonFourier1D.jl"))
 include(joinpath("..","..","src","MaxwellSolver","PoissonFourier1D.jl"))
 import .PoissonFourier1D
 
-@test solve_poisson!() == ""
+begin
+    Δx = 0.01
+    ρ = [sin(2π*i*Δx) for i = 0:1000]
+    e = similar(ρ)
+    PoissonFourier1D.solve_poisson!(e, ρ, Δx)
+    print(sum(@. (e - [-cos(2π*i*Δx)/2π for i = 0:1000])^2))
+    @test sum(@. (e - [-cos(2π*i*Δx)/2π for i = 0:1000])^2) ≈ 0 atol=1e-3
+end
