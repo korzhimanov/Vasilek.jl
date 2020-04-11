01ad75ae8044dd60939c10d83fa128807d532141
Artem Korzhimanov <korzhimanov.artem@gmail.com>
2020-04-11 22:35:09 +0300

    Rewrite PoissonFourrier1D to generate a solver with parameters closed in

--- src/MaxwellSolver/PoissonFourier1D.jl
+++ src/MaxwellSolver/PoissonFourier1D.jl
@@ -1,14 +1,31 @@
 module PoissonFourier1D
-export solve_poisson!
+export make_solve_poisson
 
-using FFTW
+using FFTW, LinearAlgebra
 
-function solve_poisson!(e, ρ, Δx)
-    F = FFTW.rfft(ρ)
+function generate_solve(ρ₀, Δx)
+    PF = FFTW.plan_rfft(ρ₀)
+    F = FFTW.rfft(ρ₀)
     ω = collect(LinRange(0.0, π/Δx, length(F)))
     ω[1] = ω[2]
-    φ = FFTW.irfft(F./(-ω.^2), length(ρ))
-    e[:] = vcat(0.5*[φ[2]-φ[end]], 0.5*(φ[3:end] - φ[1:end-2]), 0.5*[φ[1]-φ[end-1]])./Δx
+    ω⁻² = -1.0./(ω.^2)
+    Pφ = FFTW.plan_irfft(F, length(ρ₀))
+    φ = FFTW.irfft(F, length(ρ₀))
+    function solve!(e::Vector{T}, ρ::Vector{T}) where {T}
+        mul!(F, PF, ρ)
+        F[1] = 0.0 + 0.0im
+        for i = 2:length(F)
+            F[i] = ω⁻²[i] * F[i]
+        end
+        mul!(φ, Pφ, F)
+        e[1] = 0.5*(φ[2]-φ[end])/Δx
+        for i = 2:length(e)-1
+            e[i] = 0.5*(φ[i+1] - φ[i-1])/Δx
+        end
+        e[end] = 0.5*(φ[1]-φ[end-1])/Δx
+        return e
+    end
+    return solve!
 end
 
 end
--- test/MaxwellSolver/test_poisson_fourier_1d.jl
+++ test/MaxwellSolver/test_poisson_fourier_1d.jl
@@ -5,7 +5,7 @@ begin
     Δx = 0.01
     ρ = [sin(2π*i*Δx) for i = 0:1000]
     e = similar(ρ)
-    PoissonFourier1D.solve_poisson!(e, ρ, Δx)
-    print(sum(@. (e - [-cos(2π*i*Δx)/2π for i = 0:1000])^2))
+    solve! = PoissonFourier1D.generate_solve(ρ, Δx)
+    solve!(e, ρ)
     @test sum(@. (e - [-cos(2π*i*Δx)/2π for i = 0:1000])^2) ≈ 0 atol=1e-3
 end
