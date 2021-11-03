0ada6d40b21534a0d890e77905214d7478cd5e4d
Artem Korzhimanov <korzhimanov.artem@gmail.com>
2021-11-03 20:51:10 +0300

    add PML to 1D FDTD

--- src/MaxwellSolver/FDTD1D.jl
+++ src/MaxwellSolver/FDTD1D.jl
@@ -12,38 +12,63 @@ struct YeeMesh1D{T<:AbstractFloat, S<:Integer}
     end
 end
 
-function make_advance_fields(f::YeeMesh1D{T,S}, cfl, pulse_shape, Δt, Δx, x_min) where {T,S}
+struct PML{T<:Integer, S<:AbstractFloat}
+    N::T
+    σ_max::S
+    r₁::Vector{S}
+    r₂::Vector{S}
+    function PML(N::T, σ_max::S, Δx, Δt) where {T, S}
+        σ = [σ_max*(i/2N)^3 for i = 1:2N]
+        r₁ = exp.(-Δt.*σ)
+        r₂ = (1.0 .- r₁)./(Δx.*σ)
+        new{T,S}(N, σ_max, r₁, r₂)
+    end
+end
+
+function make_advance_fields(f::YeeMesh1D{T,S}, cfl, pulse_shape, Δt, Δx, x_min, pml::PML = PML(10,1e3,Δt,Δx)) where {T,S}
     Nx = f.N
 
     function generate_fields_x_min!(t)
-        f.ey[2] -= cfl*pulse_shape["y"].(t, x_min + Δx)
-        f.ez[2] -= cfl*pulse_shape["z"].(t, x_min + Δx)
+        f.ey[pml.N+2] -= cfl*pulse_shape["y"].(t, x_min + Δx)
+        f.ez[pml.N+2] -= cfl*pulse_shape["z"].(t, x_min + Δx)
         
-        f.hz[2] -= cfl*pulse_shape["y"].(t + 0.5*Δt, x_min + 1.5*Δx)
-        f.hy[2] -= cfl*pulse_shape["z"].(t + 0.5*Δt, x_min + 1.5*Δx)
+        f.hz[pml.N+2] -= cfl*pulse_shape["y"].(t + 0.5*Δt, x_min + 1.5*Δx)
+        f.hy[pml.N+2] -= cfl*pulse_shape["z"].(t + 0.5*Δt, x_min + 1.5*Δx)
     end
 
     function update_ey!()
-        for i = 2:Nx
+        for i = 2:pml.N+1
+            f.ey[i] = pml.r₁[1+2*(pml.N-i+1)]*f.ey[i] - pml.r₂[1+2*(pml.N-i+1)]*(f.hz[i] - f.hz[i-1])
+        end
+        for i = pml.N+2:Nx-pml.N
             f.ey[i] -= cfl*(f.hz[i] - f.hz[i-1])
+        end
+        for i = Nx-pml.N+1:Nx
+            f.ey[i] = pml.r₁[1+2*(i-Nx+pml.N-1)]*f.ey[i] - pml.r₂[1+2*(i-Nx+pml.N-1)]*(f.hz[i] - f.hz[i-1])
         end
     end
     
     function update_ez!()
-        for i = 2:Nx
+        for i = pml.N+2:Nx-pml.N
             f.ez[i] += cfl*(f.hy[i] - f.hy[i-1])
         end
     end
     
     function update_hy!()
-        for i = 1:Nx
+        for i = pml.N+1:Nx-pml.N
             f.hy[i] += cfl*(f.ez[i+1] - f.ez[i])
         end
     end
     
     function update_hz!()
-        for i = 1:Nx
+        for i = 1:pml.N
+            f.hz[i] = pml.r₁[2*(pml.N-i+1)]*f.hz[i] - pml.r₂[2*(pml.N-i+1)]*(f.ey[i+1] - f.ey[i])
+        end
+        for i = pml.N+1:Nx-pml.N
             f.hz[i] -= cfl*(f.ey[i+1] - f.ey[i])
+        end
+        for i = Nx-pml.N+1:Nx
+            f.hz[i] = pml.r₁[2*(i-Nx+pml.N)]*f.hz[i] - pml.r₂[2*(i-Nx+pml.N)]*(f.ey[i+1] - f.ey[i])
         end
     end
 
--- test/MaxwellSolver/test_fdtd_1d.jl
+++ test/MaxwellSolver/test_fdtd_1d.jl
@@ -16,17 +16,17 @@ function test_fdtd_1d_propagation(Δx, Δt, cfl, f₀, f₁, exp_norm_dev, args.
 
     f = deepcopy(f₀)
 
-    advance_fields! = FDTD1D.make_advance_fields(f, cfl, pulse_shape, Δt, Δx, 0)
+    advance_fields! = FDTD1D.make_advance_fields(f, cfl, pulse_shape, Δt, Δx, 0, FDTD1D.PML(0,1.0,Δx,Δt))
     
     for t in 1:10
         advance_fields!(0, :Nothing)
     end
 
     if plot_needed
-        plot!(collect(11:f.N-9), f.ey[11:end-10], label = "calculated")
+        plot!(f.ey, label = "calculated")
     end
 
-    s = sum(@. (f.ey[11:end-10] - f₁.ey[11:end-10])^2)
+    s = sum(@. (f.ey[10:end-1] - f₁.ey[10:end-1])^2)
     println("FDTD1D propagation $args $(values(values(kwargs))): $(s)")
     @test s ≈ 0 atol=exp_norm_dev
 
@@ -43,7 +43,7 @@ function test_fdtd_1d_generation(Δx, Δt, cfl, f₀, f₁, pulse_shape, exp_nor
 
     f = deepcopy(f₀)
 
-    advance_fields! = FDTD1D.make_advance_fields(f, cfl, pulse_shape, Δt, Δx, 0)
+    advance_fields! = FDTD1D.make_advance_fields(f, cfl, pulse_shape, Δt, Δx, 0, FDTD1D.PML(0,1.0,Δx,Δt))
     
     for t in 1:100
         advance_fields!(t*Δt, :Nothing)
@@ -62,19 +62,56 @@ function test_fdtd_1d_generation(Δx, Δt, cfl, f₀, f₁, pulse_shape, exp_nor
     end
 end
 
-@testset "Test 1D FDTD solvers" begin
+function test_fdtd_1d_pml(Δx, Δt, cfl, f₀, f₁, exp_norm_dev, args...; plot_needed=false, kwargs...)
+    if plot_needed
+        plt = plot(f₁.ey, label = "expected")
+    end
+
+    shape(t,x) = 0.0
+
+    pulse_shape = Dict(
+        "y"=>shape,
+        "z"=>shape
+    )
+
+    f = deepcopy(f₀)
+
+    advance_fields! = FDTD1D.make_advance_fields(f, cfl, pulse_shape, Δt, Δx, 0, FDTD1D.PML(10,1e3,Δx,Δt))
+    
+    for t in 1:200
+        advance_fields!(0, :Nothing)
+    end
+
+    if plot_needed
+        plot!(f.ey, label = "calculated")
+    end
+
+    s = sum(@. (f.ey - f₁.ey)^2)
+    println("FDTD1D PML $args $(values(values(kwargs))): $(s)")
+    @test s ≈ 0 atol=exp_norm_dev
+
+    if plot_needed
+        display(plt)
+    end
+end
+
+# @testset "Test 1D FDTD solvers" begin
     Δx = 0.01
     Δt = 0.8*Δx
     
-    f₀ = FDTD1D.YeeMesh1D{Float64}(100)
-    f₀.ey[:] = [sin(2π*i*Δx) for i = 0:100]
-    f₀.hz[:] = [sin(2π*((i+0.5)*Δx-0.5*Δt)) for i = 0:99]
+    f₀ = FDTD1D.YeeMesh1D{Float64}(110)
+    f₀.ey[2:102] = [sin(2π*i*Δx) for i = 0:100]
+    f₀.hz[2:101] = [sin(2π*((i+0.5)*Δx-0.5*Δt)) for i = 0:99]
 
-    f₁ = FDTD1D.YeeMesh1D{Float64}(100)
-    f₁.ey[:] = [sin(2π*(i*Δx-10*Δt)) for i = 0:100]
-    f₁.hz[:] = [sin(2π*((i+0.5)*Δx-10.5*Δt)) for i = 0:99]
+    f₁ = FDTD1D.YeeMesh1D{Float64}(110)
+    f₁.ey[10:110] = [sin(2π*(i*Δx)) for i = 0:100]
+    f₁.hz[10:109] = [sin(2π*((i+0.5)*Δx-0.5*Δt)) for i = 0:99]
 
-    test_fdtd_1d_propagation(Δx, Δt, Δt/Δx, f₀, f₁, 1e-7)
+    test_fdtd_1d_propagation(Δx, Δt, Δt/Δx, f₀, f₁, 1e-3)
+
+    f₁ = FDTD1D.YeeMesh1D{Float64}(110)
+    
+    test_fdtd_1d_pml(Δx, Δt, Δt/Δx, f₀, f₁, 1e-5)
 
     f₀ = FDTD1D.YeeMesh1D{Float64}(100)
     
@@ -92,4 +129,4 @@ end
 
     test_fdtd_1d_generation(Δx, Δt, Δt/Δx, f₀, f₁, pulse_shape, 0.02)
     # plot!(f.ey, label = "calculated")
-end
+# end
